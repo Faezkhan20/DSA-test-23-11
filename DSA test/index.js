de52ef890ac498c2cdc094c112044eb5e2dcade0
Faezkhan20 <138295726+Faezkhan20@users.noreply.github.com>
2023-11-25 18:42:33 +0530

    update

---
+++ DSA test/index.js
@@ -0,0 +1,75 @@
+// Q .5//
+
+// var array   = [1,2,3,4,5]
+
+// function reverseArray(array){
+// var newarray=[]
+
+//     for(var i =array.length-1;i>=0;i--){
+
+//     newarray.push(array[i])
+//     // console.log(newarray)
+//     }
+//     return newarray
+
+// }
+// console.log(reverseArray(array))
+
+
+// q
+
+// var array=[1,2,2,3,4,4,5]
+// function removeDuplicates(array){
+//     newarray=[]
+
+    
+
+//     for(var i =0;i<array.length-1;i++){
+//         // console.log(array[i])
+//        if(array[i]!==array[i+1]){
+//         newarray.push(array[i])
+//        }
+
+//     }return newarray
+    
+   
+
+
+// }
+// console.log(removeDuplicates(array))
+
+
+// Q
+
+// var array=[2,8,5,1,9]
+
+// function findMax(array){
+//     var newarray=[]
+
+//     for(var i=1;i<array.length;i++){
+//         if(array[i]>newarray) newarray=array[i]
+//     }
+//     return newarray
+
+
+// }
+
+// console.log(findMax(array))
+
+// Q 
+// var array=[1,-2,3,4,-5]
+// function sumpositivenumber(array){
+// //   var newarray=[]
+// var max=0
+
+//    for(var i =0;i<array.length-1;i++){
+//     if(array[i]>0){
+//         // console.log(array[i])
+//         max=max+array[i]
+//     }
+//    }
+//    return max
+
+
+// }console.log(sumpositivenumber(array))
+
